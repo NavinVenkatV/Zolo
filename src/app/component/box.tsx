71ae7bc59b5ea6b32a4929b25f3aab8c0490b245
NavinVenkatV <vnavinvenkat@gmail.com>
2025-01-03 02:35:22 +0530

    init

---
+++ src/app/component/box.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Box() {
+    return (
+        <div className='border-slate-300 rounded-2xl border-2 p-7 h-[400px] w-[370px] lg:mt-0 mt-4 bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg'>
+            <div className='flex flex-col'>
+                <p className='mb-12 font-semibold text-xl'>Project Completed</p>
+                <div className='flex flex-col justify-end'>
+                    <p className='text-8xl font-light p-5'>+75</p>
+                    <hr />
+                    <h3 className='pt-3'>Successful liftoffs into the digital cosmos, each leaving a trail of satisfied clients</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Box
--- src/app/component/index2.tsx
+++ src/app/component/index2.tsx
@@ -1,21 +1,32 @@
 "use client"
 import React from 'react'
 import { motion } from 'framer-motion'
+import Box from './box'
 
 function Index2() {
     return (
-        <div className='bg-gradient-to-r from-purple-800 via-black to-blue-900'> 
-            <div className='text-white text-5xl bg-black h-screen flex justify-center items-center text-center max-h-[800px]'>
+        <div className='bg-gradient-to-r from-purple-800 via-black to-blue-900 pb-5'>
+            <div className='text-white text-2xl lg:text-5xl bg-black h-screen flex justify-center items-center text-center max-h-[800px]'>
                 <motion.div
                     initial={{ opacity: 0, scale: 0.8 }}
                     animate={{ opacity: 1, scale: 1 }}
                     transition={{ duration: 0.5 }}>
-                    <div className='max-w-5xl text-slate-300'>
-                    "Providing professional web development services to design, build, and deliver dynamic, user-friendly websites tailored to your business needs. Let’s turn your ideas into impactful digital experiences!"                    </div>
+                    <div className='max-w-5xl text-slate-300 p-4'>
+                        "Providing professional web development services to design, build, and deliver dynamic, user-friendly websites tailored to your business needs. Let’s turn your ideas into impactful digital experiences!"                    </div>
                 </motion.div>
             </div>
-            <div className='text-slate-300 text-lg  min-h-[500px] text-center items-center pt-3'>
-                O U R <span className='px-3'></span> A C H I E V E M E N T S
+            <div className='text-slate-300 text-lg  min-h-[500px] pt-3'>
+                <div className='text-center items-center text-gray-500'>O U R <span className='px-3'></span> A C H I E V E M E N T S</div>
+                <motion.div
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 1 }}>
+                    <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 p-4 lg:p-6 mt-10 justify-items-center'>
+                        <Box />
+                        <Box />
+                        <Box /> 
+                    </div>
+                </motion.div>
             </div>
         </div>
     )
